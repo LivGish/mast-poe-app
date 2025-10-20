4c8fd3f5ec55ebe8a15a2deb9456e6496e05014e
LivGish <ST10486576@vcconnect.edu.za>
2025-10-20 16:46:07 +0200

    page styling completed

--- App.tsx
+++ App.tsx
@@ -186,3 +186,81 @@ function HomeScreen(props: NativeStackScreenProps<RootStackParamList, "HomeScree
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+ 
+  container: { flex: 1, backgroundColor: "#f2e9d4", padding: 15 },
+  mainTitle: { fontSize: 28, fontWeight: "800", color: "#571310", textAlign: "center" },
+  itemCountText: { fontSize: 16, color: "#571310", textAlign: "center", marginBottom: 10,},
+  
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 18,
+    marginVertical: 10,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOpacity: 0.15,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  cardContent: { padding: 15 },
+  cardTitle: { fontSize: 20, fontWeight: "700", color: "#571310" },
+  cardDesc: { color: "#571310", fontSize: 14, marginVertical: 5 },
+  cardPrice: { color: "#571310", fontSize: 13 },
+  removeButton: {
+    backgroundColor: "#a72913",
+    padding: 10,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  removeText: { color: "#fff", fontWeight: "bold" },
+  addButton: {
+    backgroundColor: "#a72913",
+    borderRadius: 30,
+    paddingVertical: 16,
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20,
+    elevation: 4,
+  },
+  addText: { color: "#fff", fontSize: 18, fontWeight: "bold" },
+ 
+  formContainer: { backgroundColor: "#f2e9d4", padding: 20 },
+  formHeader: { fontSize: 24, color: "#571310", fontWeight: "bold", textAlign: "center", marginBottom: 20 },
+  
+  input: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+    paddingHorizontal: 12,
+    height: 50,
+    justifyContent: "center",
+    marginVertical: 8,
+  },
+ 
+  pickerWrapper: { marginVertical: 10 },
+  label: { fontSize: 15, fontWeight: "600", color: "#571310", marginBottom: 6, marginLeft: 4 },
+  pickerContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    height: 50,
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  pickerStyle: {
+    height: 50,
+    width: "100%",
+    color: "#571310",
+    fontSize: 15,
+    paddingHorizontal: 10,
+    marginTop: Platform.OS === "ios" ? -6 : -2,
+  },
+ 
+  
+  saveButton: { backgroundColor: "#a72913", padding: 15, borderRadius: 10, marginTop: 15, alignItems: "center" },
+  saveButtonText: { color: "#fff", fontWeight: "bold", fontSize: 16 },
+  cancelButton: { backgroundColor: "#a72913", alignItems: "center", marginTop: 10 },
+  cancelButtonText: { color: "#fff", fontWeight: "bold" },
+});
